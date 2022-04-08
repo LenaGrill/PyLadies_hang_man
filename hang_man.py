3086507776afebbe277c985fd58b2a1939c58bf2
Lena <mgdgri@gmail.com>
2022-04-08 22:57:25 +0200

    The current code randomly chooses one of three words.

---
+++ hang_man.py
@@ -0,0 +1,11 @@
+from random import randrange
+word_nr = randrange(3)
+
+if word_nr == 0:
+    word ="word1"
+elif word_nr == 1:
+    word ="word2"
+elif word_nr == 2:
+    word ="word3"
+
+print(word)
